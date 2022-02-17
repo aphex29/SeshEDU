8b552bbb0927f6b79b0da9d7ae56128695c74737
Thomas McNeal <59153892+t-mcneal@users.noreply.github.com>
2022-02-17 14:19:03 -0600

    Add functionality to availability components

--- src/main/java/com/seshedu/backend/BackendApplication.java
+++ src/main/java/com/seshedu/backend/BackendApplication.java
@@ -26,36 +26,14 @@ public class BackendApplication {
 			Education ed1 = new Education(2L, "DePaul University", "Bachelor of Science", "Computer Science", 2018, 2022);
 			Education ed2 = new Education(2L, "DePaul University", "Master", "Computer Science", 2022, 2024);
 			educationRepo.save(ed1);
-
 			Education result = educationRepo.findById(1L).orElseThrow(() -> new EntityNotFoundException("" + 1L));
 			System.out.println(result);
-
 			Education result2 = educationRepo.findById(1L).orElseThrow(() -> new EntityNotFoundException("" + 1L));
 			result2.setSchool("Loyola University");
 			educationRepo.save(result2);
-
+			educationRepo.save(ed2);
 			List<Education> result3 = educationRepo.findByUserId(2L).orElseThrow(() -> new EntityNotFoundException("" + 2L));
-
-
-
-
-//			educationRepo.save(ed2);
-//
-
-//			List<Education> result2 = educationRepo.findByUserId(2L).orElseThrow(() -> new EntityNotFoundException("" + 2L));
-//			System.out.println("result: " + result);
-//			System.out.println("result.school: " + result.getSchool());
-//			System.out.println("result2: " + result2);
-//
-//			// Updating database
-//			result.setSchool("Loyola University");
-//			educationRepo.save(result);
-//
-//			result = educationRepo.findById(1L).orElse(null);
-//			System.out.println("new result: " + result);
-//
-//			int edDatabaseSize = educationRepo.findAll().size();
-//			System.out.println("education database size " + edDatabaseSize); // 2
+			System.out.println(result3);
 
 
 
