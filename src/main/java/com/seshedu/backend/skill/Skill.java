7eee72490d6cb28220b6ce6e9331258aad2a7199
marekjarzabek18 <87672336+marekjarzabek18@users.noreply.github.com>
2022-02-17 19:25:18 -0600

    Update Skill.java

--- src/main/java/com/seshedu/backend/skill/Skill.java
+++ src/main/java/com/seshedu/backend/skill/Skill.java
@@ -1,10 +1,61 @@
 package com.seshedu.backend.skill;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Skill {
     @Id
+    @SequenceGenerator(
+            name = "skill_sequence",
+            sequenceName = "skill_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "skill_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
+
+    @Column(
+            name = "user_id",
+            nullable = false
+    )
+
+    private Long userId;
+
+    @Column(
+            name = "skill",
+            nullable = false
+    )
+    private String skill;
+
+    public Skill(Long userId, String hobby ) {
+        this.userId = userId;
+        this.skill = skill;
+
+    }
+
+    public Skill(){}//empty constructor
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setUserId(Long newUserId){
+        userId = newUserId;
+    }
+
+    public void setSkill(String newSkill){
+
+        skill = newSkill;
+    }
+
 }
