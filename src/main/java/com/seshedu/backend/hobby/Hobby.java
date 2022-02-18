1194f0d6a5399678b2354f24544490d33312339d
marekjarzabek18 <87672336+marekjarzabek18@users.noreply.github.com>
2022-02-17 21:55:20 -0600

    Update Hobby.java

--- src/main/java/com/seshedu/backend/hobby/Hobby.java
+++ src/main/java/com/seshedu/backend/hobby/Hobby.java
@@ -1,10 +1,60 @@
 package com.seshedu.backend.hobby;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Hobby {
     @Id
+    @SequenceGenerator(
+            name = "hobby_sequence",
+            sequenceName = "hobby_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "hobby_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
+
+    @Column(
+            name = "user_id",
+            nullable = false
+    )
+    private Long userId;
+
+    @Column(
+            name = "hobby",
+            nullable = false
+    )
+    private String hobby;
+
+    public Hobby(Long userId, String hobby ) {
+        this.userId = userId;
+        this.hobby = hobby;
+
+    }
+
+    public Hobby(){}//empty constructor
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setUserId(Long newUserId){
+        userId = newUserId;
+    }
+
+    public void setHobby(String newHobby){
+
+        hobby = newHobby;
+    }
 }
+
