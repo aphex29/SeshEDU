8b5a6d48a7e06b1a57f7f3b79385190fa806bb53
marekjarzabek18 <87672336+marekjarzabek18@users.noreply.github.com>
2022-02-17 21:59:51 -0600

    Update SessionRepository.java

--- src/main/java/com/seshedu/backend/session/SessionRepository.java
+++ src/main/java/com/seshedu/backend/session/SessionRepository.java
@@ -1,8 +1,15 @@
 package com.seshedu.backend.session;
 
+import com.seshedu.backend.education.Education;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SessionRepository extends JpaRepository<Session, Long> {
+
+
+    Optional<List<Session>> findByUserId(Long userId);
 }
