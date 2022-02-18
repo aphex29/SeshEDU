b31fe891f1fa176fd0ca3781c6132365580f72c6
Raina Huerta <rainahuerta@gmail.com>
2022-02-17 21:05:45 -0600

    Finished home component and CSS

--- src/main/frontend/src/skills/Container.js
+++ src/main/frontend/src/skills/Container.js
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Skills from './Skills';
 import axios from 'axios';
 
 function SkillsContainer(props) {
     // Add logic for objects, handling form, and axios communication w/ backend
+    const [skills, setSkills] = useState({});
+
+    useEffect(() => {
+        const getSkills = () => {
+            let skills = {
+                5: {id: 5, userId: 2, skill: "java"},
+                8: {id: 8, userId: 2, skill: "python"}
+            };
+            setSkills(skills);
+        }
+
+        getSkills();
+    }, [])
 
     return (
         <div>
-            <Skills />
+            <Skills skills={skills} />
         </div>
     );
 }
--- src/main/frontend/src/skills/Skills.js
+++ src/main/frontend/src/skills/Skills.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Skills.css';
+import Button from '../button/Button';
 
 function Skills(props) {
+
+    const getSkillsHTML = (skillsMap) => {
+        let skills = []
+        for (let key in skillsMap) {
+            let skill = skillsMap[key];
+            skills.push(<li key={skill.id}>{skill.skill}</li>)
+        };
+        return skills;
+    }
+
+
     return (
         <div>
+            <h3>Skills</h3>
+            <ul>
+                {getSkillsHTML(props.skills)}
+            </ul>
 
+            <br />
+            <Button value="EDIT" />
         </div>
     );
 }
