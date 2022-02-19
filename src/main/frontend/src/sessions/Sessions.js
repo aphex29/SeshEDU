f54f5ec9214fac258ea37b451ee99d5bb34bcaf3
Thomas McNeal <59153892+t-mcneal@users.noreply.github.com>
2022-02-19 12:34:14 -0600

    Add functionality for nearby students section

---
+++ src/main/frontend/src/sessions/Sessions.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Sessions.css';
+import Button from '../button/Button';
+
+function Sessions(props) {
+    return (
+        <div>
+            <div id="nearbyStudents">
+                <h2>Nearby Students</h2>
+
+                <img src="" alt="Not Ethan" />
+
+                <img src="" alt="Not Thomas" />
+
+                <img src="" alt="Not Raina" />
+
+                <img src="" alt="Not Clara" />
+
+                <img src="" alt="Not Marek" />
+
+                <img src="" alt="Not Patrick" />
+
+                <img src="" alt="Not Chris" />
+            </div> 
+
+            <Button value="Find Available Matches" />
+        </div>
+    );
+}
+
+export default Sessions;
